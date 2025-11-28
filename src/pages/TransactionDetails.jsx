cf37ee67cb5f2febd65b9c73a234ecda63ce5c5d
Ruhul Amin <ruhulamin824854@gmail.com>
2025-11-28 23:26:32 +0600

    Details Page Update

--- src/pages/TransactionDetails.jsx
+++ src/pages/TransactionDetails.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const TransactionDetails = () => {
   const data = useLoaderData();
   const model = data.result;
+  const navigate = useNavigate();
+
+  const totalAmount = data.totalAmount;
 
   return (
     <div className="max-w-md mx-auto bg-gradient-to-l from-sky-200 via-green-200 to-red-200 rounded-xl shadow-lg overflow-hidden mt-8 mb-8 border border-gray-200">
       <div className="relative  px-6 py-6">
         {/* 🔙 Back Button */}
         <button
-          onClick={() => (window.location.href = "/my-transactions")}
-          className="absolute top-3 left-3 bg-red-300 shadow px-3 py-1 rounded-full text-sm font-semibold text-gray-700 hover:bg-gray-100"
+          className="bg-red-300 px-2 hover:bg-white font-semibold rounded-full"
+          onClick={() => navigate("/my-transactions")}
         >
           ← Back
         </button>
+
         <h2 className="text-2xl font-bold text-center text-purple-500 mb-4">
           Transaction Details
         </h2>
@@ -45,6 +49,12 @@ const TransactionDetails = () => {
           <div className="flex justify-between border border-gray-200 p-2">
             <span className="font-semibold text-gray-700">Category :</span>
             <span className="text-gray-900 capitalize">{model.category}</span>
+          </div>
+          <div className="flex justify-between border border-gray-200 bg-gray-300 p-2 rounded-md shadow-sm">
+            <span className="font-semibold text-gray-700">
+              Total Amount in "{model.category}" ({model.type}) :
+            </span>
+            <span className="text-blue-700 font-bold">{totalAmount}$</span>
           </div>
         </div>
       </div>
